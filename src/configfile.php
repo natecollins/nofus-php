a381b5141aa3fea17edaaf233e8d764a1032c741
Nathan Collins <colli372@mail.lib.msu.edu>
2014-03-05 17:56:54 -0500

    Add placeholder functions with comments.

--- src/configfile.php
+++ src/configfile.php
@@ -41,7 +41,7 @@ name = Example
 longname = John Doe     # would parse as "John Doe"
 name2 = " Jane Doe "    # to prevent whitespace trimming, add double quotes; would parse as " Jane Doe "
 name3 = 'Jerry'         # single quotes parse as normal characters; would parse as "'Jerry'"
-words = "Quotes \"inside\" a string"    # to put double quotes inside double quotes, escpae them
+words = "Quotes \"inside\" a string"    # to put double quotes inside double quotes, escape them
 tricky = "half-quoted         # unmatched double quote parses as "\"half-quoted"
 novalue =                     # values can be left blank
 
@@ -71,6 +71,25 @@ my var = my val         # spaces are not allowed in variable identifiers
 a..b = c                # scopes cannot be blank
 .d. = e                 # start and end scope can't be blank
 
+
+ **************************************
+ * Use examples
+ **************************************
+
+$cf = new ConfigFile("test.conf");
+if ($cf->load()) {
+    $v1 = $cf->get("var1");         # get value from var1, or null if doesn't exist
+    $v9 = $cf->get("var9", 123);    # get value from var9, or 123 if doesn't exist
+
+    $mw = $cf->get("marbles.white", 1);     # get marbles.white, or 1 if doesn't exist
+    $pw = $cf->get("sql.maria.auth.pw");    # get sql.maria.auth.pw, or null if doesn't exist
+
+    $sql = $cf->get('sql.maria');           # get a scope
+    $svr = $sql->get('auth.server');        # get auth.server (from sql.maria scope), or null if doesn't exist
+
+    $bad = $cf->get('does.not.exist');      # attempt to get a non-existant scope, returns null
+}
+
 */
 
 
@@ -90,6 +109,7 @@ class ConfigFile {
     private $sBlockCommentStart;
     private $sBlockCommentEnd;
     private $sVarValDelimiter;          # first instance of this is the variable/value delmiiter
+    private $sScopeDelimiter;           # character(s) that divides scope levels
 
     // Parse rules
     //TODO
@@ -98,16 +118,63 @@ class ConfigFile {
     private $aErrors;
 
     function __construct($sFileToOpen, $sAccess="ro") {
-        $this->sFileName = 'TODO';
-        $this->sPath = 'TODO';
+        $this->sFileName = null;
+        $this->sPath = null;
         $this->iHandle = 0;
 
         $this->aLineCommentStart = array('#','//');
         $this->sBlockCommentStart = "/*";
         $this->sBlockCommentEnd = "*/";
         $this->sVarValDelimiter = "=";
+        $this->sScopeDelimiter = ".";
 
         $this->aErrors = array();
+
+        # Parse sFileToOpen, if null, then this is a scope query result
+        //TODO
+    }
+
+    /**
+     * Load in default values for certain variables/scopes. If a variable already
+     * exists (e.g. from a file that was already loaded), then this will NOT overwrite
+     * the value.
+     * @param array $aDefaults An array of "scope.variable"=>"default value" pairs.
+     */
+    public function preload($aDefaults) {
+        //TODO
+    }
+
+    /**
+     * Attempt to open and parse the config file.
+     * @return boolean True if the file loaded without errors, false otherwise
+     */
+    public function load() {
+        # If file is null, then this is a scope query result, do nothing
+        if ($this->sFileName === null) {
+            $this->aErrors[] = "Cannot load file; no file was given. (Note: you cannot load() a query result.)";
+            return false;
+        }
+
+        //TODO
+    }
+
+    /**
+     * Get a list of errors when attempting to load() the file
+     * @return array And array of errors; can be empty if no errors were encountered or the file has not been loaded yet
+     */
+    public function errors() {
+        //TODO
+    }
+
+    /**
+     * Query the config for a scope/variable. Returns the value or scope on success,
+     * or mDefault (default: null) if the query was not found.
+     * @param string $sQuery The query string. e.g. "variable", "scope", "scope.variable", etc
+     * @param mixed $mDefault The return value should the query not find anything.
+     * @return string|null The matching value from the query, or mDefault if not found
+     */
+    public function get($sQuery, $mDefault=null) {
+        //TODO
     }
 
 }
